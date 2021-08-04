02c4aa09468a11ee285e918721d04385d718dea6
monkmakes <46439220+monkmakes@users.noreply.github.com>
2021-08-04 16:44:14 +0100

    Update pxt.json, main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,99 @@
-basic.forever(function () {
-	
-})
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+
+/**
+ * MonkMakes 7-Segment for micro:bit blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace sevenSegment {
+    /**
+     * Start communication with the 7-segment display using Pin0
+     * You will probably want to put this in an on start block
+     */
+    //% block
+    export function startSevenSegPin0(): void {
+        serial.redirect(
+            SerialPin.P0,
+            SerialPin.P16,
+            BaudRate.BaudRate9600
+        )
+    }
+
+    /**
+     * Start communication with the 7-segment display using Pin1
+     * You will probably want to put this in an on start block
+     */
+    //% block
+    export function startSevenSegPin1(): void {
+        serial.redirect(
+            SerialPin.P1,
+            SerialPin.P16,
+            BaudRate.BaudRate9600
+        )
+    }
+
+    /**
+     * Start communication with the 7-segment display using Pin2
+     * You will probably want to put this in an on start block
+     */
+    //% block
+    export function startSevenSegPin2(): void {
+        serial.redirect(
+            SerialPin.P2,
+            SerialPin.P16,
+            BaudRate.BaudRate9600
+        )
+    }
+
+
+    /**
+     * Send a string to the display.
+     * Only the last 4 characters will be displayed
+     */
+    //% block
+    export function writeString(value: string): void {
+        serial.writeString(",    ")
+        serial.writeString(value)
+        serial.writeString(",")
+        basic.pause(75)
+    }
+
+    /**
+     * Send a string to the display.
+     * The string will scroll slowly across the screen
+     */
+    //% block
+    export function scrollString(value: string, delay: number): void {
+        serial.writeString("/") // put into non-buffered mode
+        for (let i = 0; i < value.length; i++) {
+            serial.writeString(value.charAt(i))
+            basic.pause(delay)
+        }
+    }
+
+    /**
+     * Send a number to the display.
+     * Only the last 4 digits will be displayed
+     * You can use a single decimal point in the number
+     */
+    //% block
+    export function writeNumber(value: number): void {
+        serial.writeString(",    ")
+        serial.writeNumber(value)
+        serial.writeString(",")
+        basic.pause(75)
+    }
+
+
+    /**
+     * Clear the display.
+     */
+    //% block
+    export function clear(): void {
+        serial.writeString("/")
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+input.onButtonPressed(Button.A, function () {
+    x = 0
+})
+let x = 0
+sevenSegment.startSevenSegPin0()
+basic.forever(function () {
+    x += 1
+    sevenSegment.writeNumber(x)
+    basic.pause(1000)
+})
